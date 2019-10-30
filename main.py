6bd05bd094b8237091c47236ab718b54831e9b4b
hd-desktop <hungrysoul9@gmail.com>
2019-10-30 18:13:13 +0900

    그냥 기본으로 출력하자;;

--- main.py
+++ main.py
@@ -28,30 +28,6 @@ def main(settings):
             torrentwal.search(keyword)
 
 
-def _get_traceback_str():
-    '''
-    from http://egloos.zum.com/mcchae/v/11018564
-    '''
-
-    lines = traceback.format_exc().strip().split('\n')
-    rl = [lines[-1]]
-    lines = lines[1:-1]
-    lines.reverse()
-    nstr = ''
-    for i in range(len(lines)):
-        line = lines[i].strip()
-        if line.startswith('File "'):
-            eles = lines[i].strip().split('"')
-            basename = os.path.basename(eles[1])
-            lastdir = os.path.basename(os.path.dirname(eles[1]))
-            eles[1] = '%s/%s' % (lastdir,basename)
-            rl.append('^\t%s %s' % (nstr,'"'.join(eles)))
-            nstr = ''
-        else:
-            nstr += line
-    return '\n'.join(rl)
-
-
 if __name__ == "__main__":
     try:
         settings = json_to_namedtuple('.settings.json')
@@ -60,4 +36,4 @@ if __name__ == "__main__":
     except Exception as ex:
         token: str = settings.telegram.token
         chat_id: str = settings.telegram.chat_id
-        telegram.send_message(token, chat_id, f'exception={ex}|callstack={_get_traceback_str()}')
+        telegram.send_message(token, chat_id, f'exception={traceback.format_exc()}')
